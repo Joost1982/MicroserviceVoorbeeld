4b263a86ac01c8526e2a9bf23804526bda699dde
Joost van der Horst <jvanderhorst@ovotrack.nl>
2021-10-27 10:32:55 +0200

    added API Gateway (Nginx) + verbinding van Plaform Service naar Command Service

--- PlatformService/PlatformService/Startup.cs
+++ PlatformService/PlatformService/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using PlatformService.Data;
+using PlatformService.SyncDataServices.Http;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -34,6 +35,8 @@ namespace PlatformService
 
             services.AddScoped<IPlatformRepo, PlatformRepo>();
 
+            services.AddHttpClient<ICommandDataClient, HttpCommandDataClient>();
+
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
             services.AddControllers();
@@ -41,6 +44,8 @@ namespace PlatformService
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "PlatformService", Version = "v1" });
             });
+
+            Console.WriteLine($"--> Command Service endpoint: {Configuration["CommandService"]}");
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ PlatformService/PlatformService/SyncDataServices/Http/HttpCommandDataClient.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Configuration;
+using PlatformService.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace PlatformService.SyncDataServices.Http
+{
+    public class HttpCommandDataClient : ICommandDataClient
+    {
+        private readonly HttpClient _httpclient;
+        private readonly IConfiguration _configuration;
+
+        public HttpCommandDataClient(HttpClient httpClient, IConfiguration configuration)
+        {
+            _httpclient = httpClient;
+            _configuration = configuration;
+        }
+        
+        public async Task SendPlatformToCommand(PlatformReadDto plat)
+        {
+            var httpContent = new StringContent(
+                JsonSerializer.Serialize(plat),
+                Encoding.UTF8,
+                "application/json"
+                );
+
+            Console.WriteLine("debuuuuuuug: " + _configuration["CommandService"]);
+            var response = await _httpclient.PostAsync($"{_configuration["CommandService"]}", httpContent);
+
+            if (response.IsSuccessStatusCode) 
+            { 
+                Console.WriteLine("--> Sync POST to Command Service Ok"); 
+            } else
+            {
+                Console.WriteLine("--> Sync POST to Command Service not Ok"); 
+            }
+        }
+    }
+}
